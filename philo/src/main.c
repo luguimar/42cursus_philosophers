4f93234d15c79ad10bee6c7d602e76b688030c99
Luis Gustavo Guimaraes Cavalcante Maia De Sousa <luguimar@c2r2s5.42porto.com>
2024-04-02 21:39:30 +0100

    divided init_philo function

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: luguimar <luguimar@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 03:30:28 by luguimar          #+#    #+#             */
-/*   Updated: 2024/04/02 06:37:01 by luguimar         ###   ########.fr       */
+/*   Updated: 2024/04/02 20:44:29 by luguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,60 @@ int	ft_atoi(const char *str)
 	return (res);
 }
 
-int	init_philo(t_table *table)
+void	*routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->id % 2)
+		usleep(100);
+	while (1)
+	{
+		if (philo->table->someone_dead)
+			return (NULL);
+		take_forks(philo);
+		eat(philo);
+		put_forks(philo);
+		if (philo->table->nb_eat != -1 && philo->nb_meals >= philo->table->nb_eat)
+			return (NULL);
+		sleep_philo(philo);
+	}
+	return (NULL);
+}
+
+int	start_simulation(t_table *table)
 {
 	int	i;
 
+	table->start_time = get_time();
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		if (pthread_create(&table->philos[i].thread, NULL, &routine,
+				&table->philos[i]))
+			return (1);
+		i++;
+	}
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		if (pthread_join(table->philos[i].thread, NULL))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	init_philo_extra(int i, t_table *table)
+{
+	table->philos[i].id = i + 1;
+	table->philos[i].nb_meals = 0;
+	table->philos[i].last_meal = 0;
+	table->philos[i].table = table;
+}
+
+int	init_philo(t_table *table, int i)
+{
 	table->forks = malloc(sizeof(int) * table->nb_philo);
 	if (!table->forks)
 		return (1);
@@ -62,16 +112,11 @@ int	init_philo(t_table *table)
 		free(table->forks_mutex);
 		return (1);
 	}
-	i = 0;
-	while (i < table->nb_philo)
+	while (++i < table->nb_philo)
 	{
-		table->forks[i] = 1;
+		table->forks[i] = 0;
 		pthread_mutex_init(&table->forks_mutex[i], NULL);
-		table->philos[i].id = i + 1;
-		table->philos[i].nb_meals = 0;
-		table->philos[i].last_meal = 0;
-		table->philos[i].table = table;
-		i++;
+		init_philo_extra(i, table);
 	}
 	table->someone_dead = 0;
 	table->start_time = get_time();
@@ -88,8 +133,8 @@ int	parse_args(t_table *table, int argc, char **argv)
 		table->nb_eat = ft_atoi(argv[5]);
 	else
 		table->nb_eat = -1;
-	if (table->nb_philo < 1 || table->time_to_die < 0 || table->time_to_eat < 0
-		|| table->time_to_sleep < 0 || (argc == 6 && table->nb_eat < 0))
+	if (table->nb_philo < 1 || table->time_to_die < 1 || table->time_to_eat < 1
+		|| table->time_to_sleep < 1 || (argc == 6 && table->nb_eat < 0))
 		return (1);
 	return (0);
 }
@@ -108,7 +153,7 @@ int	main(int argc, char **argv)
 		printf("Error: Invalid arguments\n");
 		return (1);
 	}
-	if (init_philo(&table))
+	if (init_philo(&table, -1))
 	{
 		printf("Error: Failed to initialize philosophers\n");
 		return (1);
